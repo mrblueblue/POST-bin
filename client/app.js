44b7bcf3c8b69ddfd9afa529c2dda81d1bdd4086
Jonathan Huang <grokut@gmail.com>
2015-03-29 20:24:01 -0700

    basic front-end page

--- client/app.js
+++ client/app.js
@@ -1,3 +1,13 @@
-'use strict';
 
-angular.module('PostBin', []);
+var app = angular.module('PostBin', ['ngMaterial']);
+
+app.config(function($mdThemingProvider) {
+ 	$mdThemingProvider.theme('default')
+		.primaryPalette('indigo')
+		.accentPalette('blue-grey');
+});
+
+app.controller('AppController', function($scope){
+	$scope.data = "Hello World";
+})
+
