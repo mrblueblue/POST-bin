2a0cdc0a9dc44bd9601f8487c481b15c4c6242ae
jackchi <jack@smartthings.com>
2015-03-31 13:21:24 -0700

    (fix) Server can configure Logging Levels

--- logger.js
+++ logger.js
@@ -1,9 +1,39 @@
-var winston = require('winston');
+var winston = require('winston'), 
+		config = require('./config');
 
-module.exports = logger = module.exports = new (winston.Logger)({
-  transports: [
-    new (winston.transports.Console)({
-      colorize: 'all'
-    })
-  ]
+var logger = new (winston.Logger)({
+	levels: {
+    trace: 0,
+    input: 1,
+    verbose: 2,
+    prompt: 3,
+    debug: 4,
+    info: 5,
+    data: 6,
+    help: 7,
+    warn: 8,
+    error: 9
+  },
+  colors: {
+    trace: 'magenta',
+    input: 'grey',
+    verbose: 'cyan',
+    prompt: 'grey',
+    debug: 'blue',
+    info: 'green',
+    data: 'grey',
+    help: 'cyan',
+    warn: 'yellow',
+    error: 'red'
+  }
 });
+
+logger.add(winston.transports.Console, {
+  level: config.logging.level,
+  prettyPrint: true,
+  colorize: true,
+  silent: false,
+  timestamp: false
+});
+
+module.exports = logger; 
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-var express = require('express');
-var logger = require('./logger.js');
-var bodyParser = require('body-parser');
+'use strict';
+
+var express 	 = require('express'),
+		bodyParser = require('body-parser'),
+		morgan 	 = require('morgan');
+
+var logger = require('./logger.js'),
+		config = require('./config.js');
 
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var port = config.server.port; 
 
-// Initialize Server
-server.listen(3000, function(){
-  var host = server.address().address;
-  var port = server.address().port;
-  logger.info('app listening at http://%s:%s', host, port);
-});
+// EXPRESS CONFIGRATIONS
+app.use(morgan('dev'));
 
 // Serve Angular Client
 app.use(express.static(__dirname + '/client/'));
@@ -21,7 +23,15 @@ app.use(bodyParser.json());
 
 // Routes
 app.post('/postbin', function(req, res){
-  logger.info("headers", req.headers);
+	logger.warn('warning');
+  logger.data("body", req.body);
+  logger.trace('headers', req.headers);
   io.emit('news', {headers: req.headers, body: req.body});
-  res.status(202).send('POST received!')
+  res.status(202).send('POST received!');
+});
+
+// Initialize Server
+server.listen(port, function(){
+  var host = server.address().address;
+  logger.info('app listening at http://%s:%s', host, port);
 });
